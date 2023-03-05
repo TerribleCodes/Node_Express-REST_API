f1b6bb8e7acece7b34e7706417eed1545ebe6c1f
TerribleCodes <madawadesilva9998@gmail.com>
2023-03-05 20:25:17 +0530

    Added routes

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ dotenv.config();
 app.use(bodyParser.json());
 app.use("/users", userRoute);
 
+app.use("*", (req, res) =>
+  res.status(404).json({ error: "Page does not exist." })
+);
+
 app.get("/", (req, res) => {
   res.send("Homepage");
 });
--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,38 @@
 import express from "express";
 const router = express.Router();
+import { v4 as uuidv4 } from "uuid";
 
 /*
     All the routes here starts with http://localhost:5000/users
 */
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25,
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 45,
+  },
+];
+
 router.get("/", (req, res) => {
-  res.send("ree");
+  res.send(users);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body; // Fetch the user data from the req.body
+  users.push({ id: uuidv4(), ...user }); // Insert the unique user id to the user object and Push the newly created user with the id
+  res.send(`User ${user.firstName} was added to the database`);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const userwithid = users.find((user) => user.id === id);
+  res.send(userwithid);
 });
 
 export default router;
